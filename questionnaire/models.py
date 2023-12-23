e16c70147e1edd579e869aa034af4a4bb1d02254
Felipe Seiberlich <philzaiber@outlook.com>
2023-12-23 18:55:50 +0000

    Add models to questionnaire app.

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+YES_OR_NO = [("Yes", "Yes"), ("No", "No")]
+GENDER = [("Male", "Male"), ("Female", "Female"), ("Unknown", "Unknown")]
+
+class Questionnaire(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    age = models.IntegerField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER, max_length=25, null=True, blank=True)
+    disability = models.CharField(choices=YES_OR_NO, max_length=3, null=False, blank=False)
+    previous_experience = models.CharField(choices=YES_OR_NO, max_length=3 null=False, blank=False)
+    previous_training = models.CharField(choices=YES_OR_NO, null=False, blank=False)
+    physicians_name = models.CharField(max_length=75, null=False, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
