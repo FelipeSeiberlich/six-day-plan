d5b8be742095364f60c9e04f43a43869e343a39f
Felipe Seiberlich <philzaiber@outlook.com>
2023-12-29 20:08:56 +0000

    Create about.html file and url path.

--- my_project_four/urls.py
+++ my_project_four/urls.py
@@ -23,6 +23,7 @@ from questionnaire.views import questionnaire
 
 
 urlpatterns = [
+    path("about/", include("about.urls"), name="about-urls"),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
