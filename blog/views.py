145f0b9ab0f29139f7736688142fa3832c47cfe8
Felipe Seiberlich <philzaiber@outlook.com>
2023-12-31 13:00:52 +0000

    Add comment and submit it.

--- blog/views.py
+++ blog/views.py
@@ -29,12 +29,23 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
 
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+
+    comment_form = CommentForm()
+
     return render(
         request,
         "blog/post_detail.html",
         {"post": post,
         "comments": comments,
-        "comment_count": comment_count, 
+        "comment_count": comment_count,
+        "comment_form": comment_form, 
         "coder": "Felipe Seiberlich"}, 
         
     )
