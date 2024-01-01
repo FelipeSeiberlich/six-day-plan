923d9492c8cb2630616010cd7dd83f539ec7fe67
Felipe Seiberlich <philzaiber@outlook.com>
2024-01-01 15:38:27 +0000

    Create a contact request form.

--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import ContactForm
 
 
 def about_six(request):
@@ -7,9 +9,12 @@ def about_six(request):
     Renders the About page
     """
     about = About.objects.all().order_by('-updated_on').first()
+    contact_form = ContactForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "contact_form": contact_form},
     )
